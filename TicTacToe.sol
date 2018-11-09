f64bd3b77d93c9db254023a91535f46226196aa4
ChitraMP <chitramoripalli@gmail.com>
2018-11-09 10:45:25 +0530

    Third commit

--- TicTacToe.sol
+++ TicTacToe.sol
@@ -3,12 +3,15 @@ contract TicTacToe {
 
     uint[3][] board;
     mapping(address => uint) participantNumber;
+    mapping(uint => address) playerTrack;
+    mapping(address => bool) playerTurn;
     uint participantRegistered;
+    bool flag;
    
     struct Player
     {
         address playerId;
-        bool movePLayed;
+        // bool movePLayed;
         uint numberOfMovesPlayed;
     }
     
@@ -19,10 +22,11 @@ contract TicTacToe {
         for(uint i=0; i<3; i++)
         {
             for(uint j=0; j<3; j++)
-                board[i].push(0);
+                board[i][j] = 0;
         }
         
         participantRegistered = 0;
+        flag = true;
     }
     
     function registerPlayers()
@@ -30,11 +34,30 @@ contract TicTacToe {
     {
         participantRegistered += 1;
         participantNumber[msg.sender] = participantRegistered;
+        Player newPlayer;
+        newPlayer.playerId = msg.sender;
+        // newPlayer.movePLayed = false;
+        newPlayer.numberOfMovesPlayed = 0;
+        
+        participants.push(newPlayer);
+        if(participantRegistered == 1)
+        {
+            playerTurn[msg.sender] = true;
+        }
+        else
+        {
+             playerTurn[msg.sender] = false;   
+        }
     }
     
     function setMove(uint x, uint y)
+    //indexInRange()
+    //cannotPlayAgain()
     
     {
+        uint temp = participantNumber[msg.sender];
+        board[x][y] = temp;
+        
         
     }
 }
