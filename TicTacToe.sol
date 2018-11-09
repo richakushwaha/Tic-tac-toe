63d8e7de2e3fbaf5e4e846fb33aa7592d63b8b08
ChitraMP <chitramoripalli@gmail.com>
2018-11-09 11:01:01 +0530

    Fourth commit

--- TicTacToe.sol
+++ TicTacToe.sol
@@ -28,9 +28,25 @@ contract TicTacToe {
         participantRegistered = 0;
         flag = true;
     }
+    modifier onlyTwoPlayer()
+    {
+        require(participantRegistered <= 2, "There can only be two players");
+        _;
+    }
+    
+    modifier indexInRange(uint x1, uint y1)
+    {
+        require(x1>=0 && x1<=2 && y1>=0 && y1<=2, "Range for board location should be between 0 and 2");
+        _;
+    }
+    modifier cannotPlayAgain()
+    {
+        require(flag == playerTurn[msg.sender], "You already played your move");
+        _;
+    }
     
     function registerPlayers()
-    // onlyTwoPlayer()
+    onlyTwoPlayer()
     {
         participantRegistered += 1;
         participantNumber[msg.sender] = participantRegistered;
@@ -51,13 +67,32 @@ contract TicTacToe {
     }
     
     function setMove(uint x, uint y)
-    //indexInRange()
-    //cannotPlayAgain()
-    
+    indexInRange(x, y)
+    cannotPlayAgain()
+    // noWinner()
     {
         uint temp = participantNumber[msg.sender];
         board[x][y] = temp;
         
+        flag = !flag;
         
+        if(evaluateDiagonal(temp))
+        {
+            
+        }
+        
+    }
+    
+    function evaluateDiagonal(uint number) returns(bool)
+    {
+        if(number = board[0][0] && board[0][0]==board[1][1] && board[2][2]==board[1][1])
+        {
+            return true;
+        }
+        if(number = board[0][2] && board[0][2]==board[1][1] && board[2][0]==board[1][1])
+        {
+            return true;
+        }
+        return false;
     }
 }
